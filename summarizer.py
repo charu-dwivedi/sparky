73ba07e80a6b8320c7021ef9e05eef777134dbdc
Tanay Nathan <tanaynathan14@gmail.com>
2016-06-14 15:55:35 -0700

    Fixes summarizer style

--- summarizer.py
+++ summarizer.py
@@ -19,15 +19,15 @@ def summarize(title, user, room_name):
 Use get_transcript to get a transcript of chat messages
 """
 def get_transcript(user, room_name, days_limit=None, hours_limit=None, min_limit=None):
-	return indent_tagged(compile_messages(user, room_name, days_limit, hours_limit, min_limit).splitlines(), \
-		utils.get_emails_with_users(user, room_name).values())
+    return indent_tagged(compile_messages(user, room_name, days_limit, hours_limit, min_limit).splitlines(), \
+        utils.get_emails_with_users(user, room_name).values())
 
 """
 Use summarize to summarize chat messages in a room with NLP
 """
 def summarize(title, user, room_name, days_limit=None, hours_limit=None, min_limit=None):
-	text = compile_messages(user, room_name, days_limit, hours_limit, min_limit)
-	tt = TextTeaser()
-	return indent_tagged(tt.summarize(title, text), utils.get_emails_with_users(user, room_name).values())
+    text = compile_messages(user, room_name, days_limit, hours_limit, min_limit)
+    tt = TextTeaser()
+    return indent_tagged(tt.summarize(title, text), utils.get_emails_with_users(user, room_name).values())
 
 print summarize('cool title', 'chris', 'Hacker Squad', None, 5)
