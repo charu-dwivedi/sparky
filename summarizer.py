972e49459e10b1b1ba2bf0a356700e755c8bcc1b
chrchon <chrchon@cisco.com>
2016-06-14 06:44:59 -0700

    summarizer

--- summarizer.py
+++ summarizer.py
@@ -1,22 +1,52 @@
- #!/usr/bin/env python -W ignore
-
 import utils
 from datetime import datetime
+import pytz
+
 from textteaser import TextTeaser
 
-def compile_messages(user, room_name):
-    text = ''
-    for msg in reversed(utils.get_messages_for_user(user, room_name)['items']):
-        text += msg['text'] + '\n'
-    return text
+def tag_message(msg, tag):
+    indent = ' ' * (len(msg['personEmail']) + 2)
+    return tag + ': ' + msg['text'].replace('\n', '\n{}'.format(tag+': ')).rstrip() + '\n'
 
-def summarize(title, user, room_name):
-    text = compile_messages(user, room_name)
-    tt = TextTeaser()
-    return tt.summarize(title, text)
+def compile_messages(user, room_name, days_limit=None, hours_limit=None, min_limit=None):
+    text, email_with_users = '', utils.get_emails_with_users(user, room_name)
+    now = pytz.utc.localize(datetime.utcnow())
+    for msg in reversed(utils.get_messages_for_user(user, room_name)['items']):
+        if 'text' in msg:
+            time = pytz.utc.localize(datetime.strptime(msg['created'], '%Y-%m-%dT%H:%M:%S.%fZ'))
+            if days_limit == None and hours_limit == None and min_limit == None or \
+                days_limit != None and abs((now - time).days) <= days_limit or \
+                hours_limit != None and abs((now - time).total_seconds()) // 3600 <= hours_limit or \
+                min_limit != None and abs((now - time).seconds) // 60 <= days_limit:\
+                text += tag_message(msg, email_with_users[msg['personEmail']])
+    return text.rstrip()
+
+def indent_tagged(text, tags):
+    if text == None:
+        None
+    if tags == None:
+        return text
+    last_tag, indented = '', ''
+    for line in text:
+        tag = None
+        for t in tags:
+            if line[:len(t)] == t:
+                tag = t
+        if tag is None:
+            indented  = indented.rstrip() + line + '\n'
+            continue
+        indent = ' ' * (len(tag) + 2)
+        line = line.replace('\n{}'.format(tag+': '), '\n' + indent)
+        if last_tag == tag:
+            indented += indent + line[len(tag)+2:] + '\n'
+        else:
+            indented += line + '\n'
+            last_tag = tag
+    return indented
 
 """
 Use get_transcript to get a transcript of chat messages
+Don't use the limits. We have not been able to account for timezones
 """
 def get_transcript(user, room_name, days_limit=None, hours_limit=None, min_limit=None):
     return indent_tagged(compile_messages(user, room_name, days_limit, hours_limit, min_limit).splitlines(), \
@@ -24,10 +54,12 @@ def get_transcript(user, room_name, days_limit=None, hours_limit=None, min_limit
 
 """
 Use summarize to summarize chat messages in a room with NLP
+Don't use the limits. We have not been able to account for timezones
 """
 def summarize(title, user, room_name, days_limit=None, hours_limit=None, min_limit=None):
     text = compile_messages(user, room_name, days_limit, hours_limit, min_limit)
     tt = TextTeaser()
     return indent_tagged(tt.summarize(title, text), utils.get_emails_with_users(user, room_name).values())
 
+# print summarize('ping pong', 'chris', 'Ping Pong SJ-29', 6)
 print summarize('cool title', 'chris', 'Hacker Squad', None, 5)
--- utils.py
+++ utils.py
@@ -4,9 +4,6 @@ import json
 with open('developer_tokens.json') as data:
     developer_tokens = json.load(data)
 
-with open('suggested_users.json') as sugg:
-    sugg_users = json.load(sugg)
-
 def get_users(token, room_id):
     legit_token = "Bearer " + token
     url = 'https://api.ciscospark.com/v1/memberships'
@@ -104,56 +101,7 @@ def delete_room(token, room_id):
     delete_output_code = requests.delete(url, headers=headers, params=params)
     return delete_output_code   
 
-def check_suggested_members(member_name):
-    if member_name in sugg_users:
-        if (len(sugg_users[member_name])>1):
-            print "Did you mean one of these? "
-            count =1
-            for suggested_member in sugg_users[mem]:
-                print count + ": "+ suggested_member[0]
-                print "   " + suggested_member[1]
-            num = raw_input("Respond with a number: ")
-            return sugg_users[name][num-1][1]
-        else:
-            return sugg_users[name][0][1]
-    else:
-        return 0
-
-
-
-def find_members(token, member_input):
-    final_member_list = []
-    legit_token = "Bearer " + token
-    search_url = "https://api.ciscospark.com/v1/people"
-    for member in member_input:
-        headers = {
-            'Authorization': legit_token
-        }
-        params = {
-            'displayName': member
-        }
-        matching_members = requests.get(search_url, headers=headers, params=params).json()
-        if len(matching_members['items']) == 0:
-            print "No matching members"
-        elif len(matching_members['items']) == 1:
-            final_member_list.append(matching_members['items'][0]['email'])
-        elif len(matching_members['items']) > 5:
-            found_member = check_suggested_members((member.split())[0])
-            if found_member == 0:
-                print "Please specify " + member + " a bit more"
-            else:
-                final_member_list.append(found_member)
-        else:
-            print "Did you mean one of these? "
-            for matched_member in matching_members['items']:
-                print count + ": "+ 
-                print "   " + suggested_member[1]
-            num = raw_input("Respond with a number: ")
-
-
-
-
-def add_members_to_room(token, room_id, member_input):
+def add_members_to_room(token, room_id, room_members):
     legit_token = "Bearer " + token
     search_url = "https://api.ciscospark.com/v1/people"
     join_url = "https://api.ciscospark.com/v1/memberships"
@@ -164,15 +112,18 @@ def add_members_to_room(token, room_id, member_input):
         params = {
             'displayName': member
         }
-        count =0    
-        members_to_add = find_members(token, member_input)
-        for member_email in members_to_add:
+        count =0
+        matching_members = requests.get(search_url, headers=headers, params = params).json()
+        for matched_member in matching_members['items']:
+            count+= 1
+        if count ==1:
             add_params = {
                 'roomId': room_id,
-                'personEmail':member_email
+                'personId':matching_members['items'][0]['id']
             }
             membership_create_response = requests.post(join_url, headers=headers, data=add_params).json()
-
+        else: 
+            print "Add functionality for multiple members!"
 
 """
 Use these functions
@@ -200,16 +151,6 @@ def get_emails_with_users(user, room_name):
 
 #users = get_users(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
 #print get_messages(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
-<<<<<<< HEAD
-#print get_roomid('dockerize teamgold services', developer_tokens['tanay'])
-#print get_messages_for_user('tanay', 'dockerize teamgold services')
-
-#room_members = ["Christopher Chon", "Anjum Shaik", "Tanay Nathan"]
-#rname = "test_room"
-#room_id = create_room(developer_tokens['charu'], rname)
-#add_members_to_room(developer_tokens['charu'], room_id, room_members)
-#delete_room(developer_tokens['charu'], room_id)
-=======
 # print get_roomid('Hacker Squad', developer_tokens['chris'])
 # print get_messages_for_user('chris', 'Hacker Squad')
 
@@ -218,5 +159,4 @@ def get_emails_with_users(user, room_name):
 # room_id = create_room(developer_tokens['charu'], rname)
 # add_members_to_room(developer_tokens['charu'], room_id, room_members)
 # delete_room(developer_tokens['charu'], room_id)
->>>>>>> cfbc43b16b86cf3b90f0f71c9a9b1981525cb9b6
 
