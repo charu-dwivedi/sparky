a2e3cc8254ef979828ccdf3d8d2943d81f3e4017
chrchon <chrchon@cisco.com>
2016-06-14 14:31:27 -0700

    moved and cleaned updateroom to utils

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import json
 with open('developer_tokens.json') as data:
     developer_tokens = json.load(data)
 
+"""
+Internal Room Queries
+"""
 def get_users(token, room_id):
     legit_token = "Bearer " + token
     url = 'https://api.ciscospark.com/v1/memberships'
@@ -76,6 +79,9 @@ def get_room(room_name, token, max_msgs=float('inf'), room_type=None):
 def get_roomid(room_name, token, max_msgs=float('inf'), room_type=None):
     return get_room(room_name, token, max_msgs=float('inf'), room_type=None)['id']
 
+"""
+Internal Room Action Commands
+"""
 def create_room(token, room_name):
     legit_token = "Bearer " + token
     url = 'https://api.ciscospark.com/v1/rooms'
@@ -116,7 +122,7 @@ def add_members_to_room(token, room_id, room_members):
         matching_members = requests.get(search_url, headers=headers, params = params).json()
         for matched_member in matching_members['items']:
             count+= 1
-        if count ==1:
+        if count == 1:
             add_params = {
                 'roomId': room_id,
                 'personId':matching_members['items'][0]['id']
@@ -124,6 +130,24 @@ def add_members_to_room(token, room_id, room_members):
             membership_create_response = requests.post(join_url, headers=headers, data=add_params).json()
         else: 
             print "Add functionality for multiple members!"
+
+def change_room_name(token, old_name, new_name):
+    legit_token = "Bearer " + token
+    rooms = get_rooms(token)['items']
+    for r in rooms:
+        if r['title'] == old_name:
+            update_url = 'https://api.ciscospark.com/v1/rooms/%s' % r['id']
+            headers = {
+                'Accept': 'application/json',
+                'Authorization': legit_token
+            }
+            params = {
+                'title': new_name
+            }
+            return requests.put(update_url, headers=headers, data=params).json()
+    print 'Room \'%s\' could not be found' % old_name
+    print 'No room updated'
+    return None
 
 """
 Use these functions
@@ -146,11 +170,11 @@ def get_emails_with_users(user, room_name):
         raise Exception("User {} doesn't exist".format(user))
     return get_email_user(developer_tokens[user], get_roomid(room_name, developer_tokens[user]))
 
-# returns id of newly created room
+# creates room and returns id of newly created room
 def make_room(user, room_name):
     return create_room(developer_tokens[user], room_name)
 
-# returns output code for delete
+# removes room and returns output code for delete
 def remove_room(user, room_name):
     return delete_room(developer_tokens[user], get_roomid(room_name, developer_tokens[user]))
 
@@ -158,10 +182,14 @@ def remove_room(user, room_name):
 def add_members(user, room_name, room_members):
     add_members_to_room(developer_tokens[user], get_roomid(room_name, developer_tokens[user]), room_members)
 
+# changes an existing room's name and returns the output code (returns None if rooms doesn't exist)
+def update_room_name(user, room_name, new_room_name):
+    return change_room_name(developer_tokens[user], room_name, new_room_name)
+
 ##########################################
 
-#users = get_users(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
-#print get_messages(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
+# users = get_users(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
+# print get_messages(developer_tokens['tanay'], 'Y2lzY29zcGFyazovL3VzL1JPT00vYjhhMmFhYjAtMmU5Mi0xMWU2LTg0YWEtNWY1MGViMDZhMjAx')
 # print get_roomid('Hacker Squad', developer_tokens['chris'])
 # print get_messages_for_user('chris', 'Hacker Squad')
 
@@ -170,3 +198,6 @@ def add_members(user, room_name, room_members):
 # print make_room('charu', rname)
 # add_members('charu', rname, room_members)
 # print remove_room('charu', rname)
+
+# print update_room_name('chris', 'Hacker Squad', 'Chris Fan Club')
+# print update_room_name('chris', 'Chris Fan Club', 'Sparky')
